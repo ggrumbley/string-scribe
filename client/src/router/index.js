fea88c6ed44e1f51f1617e6f1b2dc6642a5b1c57
Gary Grumbley <g.grumbley@alphasystem.lan>
2017-10-19 14:11:48 -0700

    Add remaining components to client router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,49 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-
 import Register from '@/components/Register';
 import Login from '@/components/Login';
-import Songs from '@/components/Songs';
-
+import Songs from '@/components/Songs/Index';
+// import CreateSong from '@/components/CreateSong';
+// import EditSong from '@/components/EditSong';
+// import ViewSong from '@/components/ViewSong/Index';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/songs',
-      name: 'songs',
-      component: Songs,
-    },
-    {
       path: '/register',
       name: 'register',
       component: Register,
     },
     {
-      path: 'login',
+      path: '/login',
       name: 'login',
       component: Login,
+    },
+    {
+      path: '/songs',
+      name: 'songs',
+      component: Songs,
+    },
+    // {
+    //   path: '/songs/create',
+    //   name: 'songs-create',
+    //   component: CreateSong,
+    // },
+    // {
+    //   path: '/songs/:songId',
+    //   name: 'song',
+    //   component: ViewSong,
+    // },
+    // {
+    //   path: '/songs/:songId/edit',
+    //   name: 'song-edit',
+    //   component: EditSong,
+    // },
+    {
+      path: '*',
+      redirect: 'songs',
     },
   ],
 });
